56f607538f885b8afe8e4fa5d3d18c092c7f3b8e
Alwin Tareen <altareen@gmail.com>
2019-09-05 14:16:30 +0800

    added code

---
+++ 01JavaSyntax/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello world");
+        int average = 58;
+        double pi = 3.14159;
+        boolean lightson = true;
+        boolean sprinkleron = false;
+        String dinner = "pizza";
+    }
+    
+}
